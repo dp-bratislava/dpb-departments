e35dbae777816dbe86034b4afdf04e11dc967c53
Lubos Babocky <Babocky.Lubos@dpb.sk>
2025-08-25 09:05:42 +0200

    Initial commit..

---
+++ Providers/DepartmentsServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\ServiceProvider;
+
+class DepartmentsServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
